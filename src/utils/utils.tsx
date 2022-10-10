e1c8357b86092dab3235084f18b2011b10f35c79
vi- <reach.vctr@gmail.com>
2022-10-11 03:10:28 +1100

    case insensitive sort order

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,9 +1,9 @@
 export const sortArrayOfObjects = (arrayToSort: any[], param: string, ascending: boolean = true) => {
   const sortedArray = [...arrayToSort].sort((a: any, b: any) => {
-    if (a[param] < b[param]) {
+    if (a[param].toLowerCase() < b[param].toLowerCase()) {
       return -1
     }
-    if (a[param] > b[param]) {
+    if (a[param].toLowerCase() > b[param].toLowerCase()) {
       return 1
     }
     return 0
